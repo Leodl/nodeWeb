a5fe9c0c73df92f0747fe5b9b9e6f8a166872dc0
Leodl <dinglei@babyun.cn>
2018-01-31 10:29:00 +0800

    dl

---
+++ http.js
@@ -0,0 +1,65 @@
+var PORT = 3002;
+var http = require('http');
+var url=require('url');
+var fs=require('fs');
+var mine=require('./mine').types;
+var path=require('path');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({
+target: 'http://106.15.137.203', //接口地址
+// 下面的设置用于https
+// ssl: {
+// key: fs.readFileSync('server_decrypt.key', 'utf8'),
+// cert: fs.readFileSync('server.crt', 'utf8')
+// },
+// secure: false
+});
+proxy.on('error', function(err, req, res){
+res.writeHead(500, {
+'content-type': 'text/plain'
+});
+console.log(err);
+res.end('Something went wrong. And we are reporting a custom error message.');
+});
+var server = http.createServer(function (request, response) {
+var pathname = url.parse(request.url).pathname;
+//var realPath = path.join("main-pages", pathname); // 指定根目录
+var realPath = path.join("zhitong", pathname);
+console.log(pathname);
+console.log(realPath);
+var ext = path.extname(realPath);
+ext = ext ? ext.slice(1) : 'unknown';
+//判断如果是接口访问，则通过proxy转发
+if(pathname.indexOf("meritpay") > 0){
+proxy.web(request, response);
+return;
+}
+fs.exists(realPath, function (exists) {
+if (!exists) {
+response.writeHead(404, {
+'Content-Type': 'text/plain'
+});
+response.write("This request URL " + pathname + " was not found on this server.");
+response.end();
+} else {
+fs.readFile(realPath, "binary", function (err, file) {
+if (err) {
+response.writeHead(500, {
+'Content-Type': 'text/plain'
+});
+response.end(err);
+} else {
+var contentType = mine[ext] || "text/plain";
+response.writeHead(200, {
+'Content-Type': contentType
+});
+response.write(file, "binary");
+response.end();
+}
+});
+}
+});
+});
+server.listen(PORT);
+console.log("Server runing at port: " + PORT + ".");
